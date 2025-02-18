f23fb5110f38339a8495fcd68a688c4f67bab7df
T9code <6rooo8sy@gmail.com>
2025-02-18 17:05:53 +0100

    Initial Commit

---
+++ Musico/MusicoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicoApp.swift
+//  Musico
+//
+//  Created by MacBook on 2025-02-18.
+//
+
+import SwiftUI
+
+@main
+struct MusicoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusicoTests/MusicoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicoTests.swift
+//  MusicoTests
+//
+//  Created by MacBook on 2025-02-18.
+//
+
+import Testing
+@testable import Musico
+
+struct MusicoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
